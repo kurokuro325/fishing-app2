8904965af54de83421ae665a7d7de2c9b5023658
kurokuro325 <katapa.zari.123@gmail.com>
2022-10-18 13:02:19 +0900

    バリデーションの記述

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :lure
+  validates :title, :text, presence: true
+  validates :lure_id, numericality: { other_than: 1 , message: "can't be blank"} 
 end
