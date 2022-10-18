22e6cb9d34f0a1d199fa2c41e2113e07885d2002
kurokuro325 <katapa.zari.123@gmail.com>
2022-10-18 17:24:16 +0900

    ルーティングの記述

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   get 'posts/index'
   get 'posts/new'
   root to: 'posts#index'
-  resources :posts
+  resources :posts only: [:index, :new, :create, :destroy]
 end
