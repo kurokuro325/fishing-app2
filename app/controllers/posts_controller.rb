cefcbacaa56850863ddb86012bcef38729e036eb
kurokuro325 <katapa.zari.123@gmail.com>
2022-10-19 12:14:10 +0900

    rubocopの実行

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -46,8 +46,6 @@ class PostsController < ApplicationController
   end
 
   def move_to_index
-    unless user_signed_in?
-      redirect_to action: :index
-    end
+    redirect_to action: :index unless user_signed_in?
   end
 end
