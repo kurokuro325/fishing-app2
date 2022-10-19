9ddf3e9725f81b1eed9890d29e8e487dd32b8e02
kurokuro325 <katapa.zari.123@gmail.com>
2022-10-19 13:56:46 +0900

    投稿時にユーザー情報の追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -38,7 +38,7 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:title, :text, :lure_id, :image)
+    params.require(:post).permit(:title, :text, :lure_id, :image).merge(user_id: current_user.id)
   end
 
   def set_post
