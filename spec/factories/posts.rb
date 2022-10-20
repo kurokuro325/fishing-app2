03e96c5a7fa6cafc094aa3585988040c63bc7fea
kurokuro325 <katapa.zari.123@gmail.com>
2022-10-20 23:01:40 +0900

    rubocopの実行

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :post do
-    title              { 'abcd123' }
-    text                 { Faker::Lorem.sentence } 
+    title { 'abcd123' }
+    text                 { Faker::Lorem.sentence }
     lure_id              { 2 }
-    image              { Faker::Lorem.sentence }
+    image { Faker::Lorem.sentence }
     association :user
     after(:build) do |post|
       post.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,39 +3,35 @@ require 'rails_helper'
 RSpec.describe Post, type: :model do
   before do
     @post = FactoryBot.build(:post)
-end
+  end
 
-describe '投稿の保存' do
-  context '投稿ができる場合' do
-    it 'titleとtext、image、lure_idが入力されていれば投稿できる' do
-      expect(@post).to be_valid
+  describe '投稿の保存' do
+    context '投稿ができる場合' do
+      it 'titleとtext、image、lure_idが入力されていれば投稿できる' do
+        expect(@post).to be_valid
+      end
+    end
+    context '投稿できない場合' do
+      it 'titleが空では投稿できない' do
+        @post.title = ''
+        @post.valid?
+        expect(@post.errors.full_messages).to include("Title can't be blank")
+      end
+      it 'textが空では投稿できない' do
+        @post.text = ''
+        @post.valid?
+        expect(@post.errors.full_messages).to include("Text can't be blank")
+      end
+      it 'lure_idが空では投稿できない' do
+        @post.lure_id = ''
+        @post.valid?
+        expect(@post.errors.full_messages).to include("Lure can't be blank")
+      end
+      it 'imageが空では投稿できない' do
+        @post.image = nil
+        @post.valid?
+        expect(@post.errors.full_messages).to include("Image can't be blank")
+      end
     end
   end
-  context '投稿できない場合' do
-    it 'titleが空では投稿できない' do
-      @post.title = ''
-      @post.valid?
-      expect(@post.errors.full_messages).to include("Title can't be blank")
-    end
-    it 'textが空では投稿できない' do
-      @post.text = ''
-      @post.valid?
-      expect(@post.errors.full_messages).to include("Text can't be blank")
-    end
-    it 'lure_idが空では投稿できない' do
-      @post.lure_id = ''
-      @post.valid?
-      expect(@post.errors.full_messages).to include("Lure can't be blank")
-    end
-    it 'imageが空では投稿できない' do
-      @post.image = nil 
-      @post.valid?
-      expect(@post.errors.full_messages).to include("Image can't be blank")
-    end
-  end
 end
-end
-
-
-
-
