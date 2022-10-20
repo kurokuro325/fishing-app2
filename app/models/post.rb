382daa4dd41fcb20ca86e7baddca0e8c16858bab
kurokuro325 <katapa.zari.123@gmail.com>
2022-10-20 11:21:20 +0900

    rubocopの実行

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,7 +9,7 @@ class Post < ApplicationRecord
   def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
   end
-  
+
   validates :title, :text, presence: true
   validates :lure_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :image, presence: true
