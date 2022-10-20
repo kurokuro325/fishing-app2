1d3c87a72bb109b6dcd21250db9084d96561fbd5
kurokuro325 <katapa.zari.123@gmail.com>
2022-10-20 21:39:32 +0900

    FactoryBotの記述

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :post do
-    
+    title              { 'abcd123' }
+    text                 { Faker::Lorem.sentence }
+    lure_id              { 2 }
+    image { Faker::Lorem.sentence }
   end
 end
