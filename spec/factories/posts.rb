e0877ef11a98e11aa21f6cfddb5314d2b230e6cd
kurokuro325 <katapa.zari.123@gmail.com>
2022-10-20 23:00:33 +0900

    Postモデルの単体テストの実装

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,8 +1,12 @@
 FactoryBot.define do
   factory :post do
     title              { 'abcd123' }
-    text                 { Faker::Lorem.sentence }
+    text                 { Faker::Lorem.sentence } 
     lure_id              { 2 }
-    image { Faker::Lorem.sentence }
+    image              { Faker::Lorem.sentence }
+    association :user
+    after(:build) do |post|
+      post.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
