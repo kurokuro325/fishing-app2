57f44bdeb7492f4e878332bcc1393bdf9bf0e4d0
kurokuro325 <katapa.zari.123@gmail.com>
2022-10-20 11:09:33 +0900

    アソシエーションの記述

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,12 @@ class Post < ApplicationRecord
   belongs_to :lure
   has_many :comments
   has_one_attached :image
+  has_many :favorites, dependent: :destroy
+
+  def favorited_by?(user)
+    favorites.where(user_id: user.id).exists?
+  end
+  
   validates :title, :text, presence: true
   validates :lure_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :image, presence: true
