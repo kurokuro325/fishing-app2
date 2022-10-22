9c0a26c3ac4985eaa182549ab0eb493c33b6a67d
kurokuro325 <katapa.zari.123@gmail.com>
2022-10-22 14:14:45 +0900

    rubocopの実行

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,7 +5,6 @@ class Post < ApplicationRecord
   has_many :comments
   has_one_attached :image
   has_many :favorites, dependent: :destroy
-  
 
   def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
